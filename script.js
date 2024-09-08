03cd47e9b05106de423666c42a5938169ee1c77e
abduvokhidoff <abcdefgmananam@gmail.com>
2024-09-08 21:32:53 +0500

    done

---
+++ script.js
@@ -0,0 +1,39 @@
+// let num = prompt('Tavarni nomini yozing')
+
+// if(num==='Macbook Air M2'){
+// alert('Price:1200$')
+// }
+// else if(num=='Iphone 15 Pro Max'){
+// alert('Price:1000$')
+// }
+
+// else if(num== 'Samsung S24 Ultra'){
+// alert('Price:800$')
+// }
+ 
+// else if(num==='Iphone 14 Pro'){
+// alert('Price:670$')
+// }
+// else if(num==='Macbook Air M1'){
+// alert('Price:1100$')
+// }
+// else{
+//     alert('Tovar Topilmadi')
+// }
+let string = prompt('String typedagi malumot kiriting')
+console.log("String sozi:", string)
+
+let number = prompt('Number typedagi malumot kiriting')
+console.log("Number:", number)
+
+let booleng = prompt('Boolean typedagi malumot kiriting')
+console.log("Boolean", booleng)
+
+confirm(`Hamma Malumotarni togri kiritganingizga ishochingiz komilmi?`)
+
+alert(`Siz kiritgan ${string} sozingiz string typega tegishli\n
+    Siz kiritgan ${number} sozingiz number typega tegishli\n
+    Siz kiritgan ${booleng} sozingiz boolen typega tegishli`)
+
+
+
